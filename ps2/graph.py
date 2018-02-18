0643d7438389ed937671292510a643e6f26946e6
Whil <ncsupiratecaptain@gmail.com>
2018-02-18 16:48:49 -0700

    Done with graph.py for ps2.  Also if you don't clear the spyder compiler it remembers and fails on old unit tests.

--- ps2/graph.py
+++ ps2/graph.py
@@ -55,16 +55,18 @@ class Edge(object):
 
 class WeightedEdge(Edge):
     def __init__(self, src, dest, total_distance, outdoor_distance):
-        pass  # TODO
+        Edge.__init__(self, src, dest)
+        self.total_distance = total_distance
+        self.outdoor_distance = outdoor_distance
 
     def get_total_distance(self):
-        pass  # TODO
+        return self.total_distance
 
     def get_outdoor_distance(self):
-        pass  # TODO
+        return self.outdoor_distance
 
     def __str__(self):
-        pass  # TODO
+        return "{s}->{d} ({td}, {od})".format(s = str(self.get_source()), d = str(self.get_destination()), td = self.get_total_distance(), od = self.get_outdoor_distance())
 
 
 class Digraph(object):
@@ -90,13 +92,20 @@ class Digraph(object):
     def add_node(self, node):
         """Adds a Node object to the Digraph. Raises a ValueError if it is
         already in the graph."""
-        pass  # TODO
+        if node in self.nodes:
+            raise ValueError
+        else:
+            self.nodes.add(node)
+            self.edges[node] = []
 
     def add_edge(self, edge):
         """Adds an Edge or WeightedEdge instance to the Digraph. Raises a
         ValueError if either of the nodes associated with the edge is not
         in the  graph."""
-        pass  # TODO
+        if not (edge.get_source() in self.nodes and edge.get_destination() in self.nodes):
+            raise ValueError
+        else:
+            self.edges[edge.get_source()].append(edge)
 
 
 # ================================================================
